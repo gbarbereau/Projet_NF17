b8c2509d97ac215ff7e3172867426f943cfa54fb
Gautier BARBEREAU <gautier.barbereau@gmail.com>
2016-05-12 11:51:59 +0200

    Création connexion.php

---
+++ connexion.php
@@ -0,0 +1,8 @@
+<?php
+	$vHost= "tuxa.sme.utc";
+	$vPort= "5432";
+	$vData = "dbnf17p115";
+	$vUser = "nf17p115";
+	$vPass = "hXtVa0yM";
+	$vConn = pg_connect("host=$vHost port=$vPort dbname=$vData user=$vUser password=$vPassword");
+?>
