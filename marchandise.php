9b12f1fea04e70bdf2f44c9cd0a4d53ffdbabdae
ConstiRenard <constance.jacques@etu.utc.fr>
2016-05-19 16:20:20 +0200

    ajout de quelques lignes de code :p

--- marchandise.php
+++ marchandise.php
@@ -1,1 +1,22 @@
-//repond aux requetes rupture et ajouter du stock
+<html>
+<body>
+<p>
+<?php
+	//repond aux requetes rupture et ajouter du stock
+	
+	//connexion
+	$vHost = "tuxa.sme.utc";
+	$vPort = "5432" ;
+	$vData = "dbnf17p114";
+	$vUser = "nf17p114";
+	$vPass = "SVYhc0nK";
+	$vConn=pg_connect("host=$vHost port=$vPort dbname=$vData user=$vUser password=$vPass");
+
+	//REQUETE 1 : 
+	//permet de donner les marchandises qui sont en rupture de stock
+	$vSql=("SELECT identifiant, denomination FROM Marchandise WHERE stock=NULL");
+?>
+</p>
+
+</body>
+</html>
