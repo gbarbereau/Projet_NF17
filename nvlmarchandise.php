d9f0cc44d13ad00762849a53502f9d39d54c2c80
ConstiRenard <constance.jacques@etu.utc.fr>
2016-05-20 15:56:09 +0200

    Finition de ce fichier qui contient un formulaire permettant d'ajouter de nouvelles marchandises

---
+++ nvlmarchandise.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<p>
+<a href='marchandise.php'>  &lsaquo; Retour à la page précédente</a>
+</p>
+<p>
+<form method="post" action="ajoutermarchandise.php">
+	<center><h2>Ajout d'une nouvelle marchandise</h2></center>
+	<p>Entrez l'identifiant de la marchandise : <input type="text" name="id"/></p>
+	<p>Entrez sa dénomination : <input type="text" name="den"/></p>
+	<p>Entrez son prix : <input type="text" name="prix"/></p>
+	<p>Entrez son stock : <input type="text" name="stock"/></p>
+	<p>Entrez le numéro du client associé : <input type="text" name="numclient"/></p>
+
+	<p><input type="submit"/></p>
+</form>
+
+</p>
+</body>
+</html>
