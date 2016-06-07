4fbd62aa9585685a037ac1126170393c5977e3f9
ConstiRenard <constance.jacques@etu.utc.fr>
2016-06-07 16:00:27 +0200

    modification des input pour le numero de client : devient maintenant une liste deroulante

--- nvlmarchandise.php
+++ nvlmarchandise.php
@@ -9,8 +9,20 @@
 	<p>Entrez l'identifiant de la marchandise : <input type="text" name="id"/></p>
 	<p>Entrez sa dénomination : <input type="text" name="den"/></p>
 	<p>Entrez son prix : <input type="text" name="prix"/></p>
+	<p>Rappel : le prix est un entier</p>
 	<p>Entrez son stock : <input type="text" name="stock"/></p>
-	<p>Entrez le numéro du client associé : <input type="text" name="numclient"/></p>
+	<p>Entrez le numéro du client associé : </p>
+	<select name="num_client" id="num_client">
+	<?php
+	include 'connexion.php';
+
+	$vSql="SELECT num_client,nom, prenom FROM Client";
+	$vQuery=pg_query($vConn, $vSql);
+	  while ($vResult = pg_fetch_array($vQuery, null, PGSQL_ASSOC)) {
+	  	echo "<option value='$vResult[num_client]'>$vResult[num_client] - $vResult[prenom] $vResult[nom]</option>";
+}	
+	?>
+	</select>
 
 	<p><input type="submit"/></p>
 </form>
